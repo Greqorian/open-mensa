eb2e953c8a7bc56294b9746f99741c562b06a35c
Greq <greqorian@gmail.com>
2022-05-05 23:02:10 +0200

    feat: add onclick events

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 
+import { useState } from 'react'
 import { useQuery } from 'react-query'
 
 import Card from './Card'
@@ -8,24 +9,40 @@ import Spinner from './Spinner'
 
 const LandingPage = () => {
 
+    const lat = 52.393535;
+    const lng = 13.127814;
+    const dist = 5;
+
+    const URL = `https://openmensa.org/api/v2/canteens?near[lat]=${lat}&near[lng]=${lng}&near[dist]=${dist}`;
+
+    const [query, setQuery] = useState(URL);
+
     const { isLoading, isError, data, error } = useQuery('companyData', () =>
-        fetch(`https://openmensa.org/api/v2/canteens?near[lat]=52.393535&near[lng]=13.127814&near[dist]=5`).then((res) =>
+        fetch(query).then((res) =>
             res.json()
         )
     )
 
-    if (isLoading) {return <Spinner />}
-    if (isError) {return <ErrorMessage errorMsg={error.message}/>}
+    const handleQuery = (id) => {
+        
+        setQuery(`https://openmensa.org/api/v2/canteens/${id}`)
+        console.log(query);
+    };
+
+    if (isLoading) { return <Spinner /> }
+    if (isError) { return <ErrorMessage errorMsg={error.message} /> }
 
     return (
         <Container>
             <ContainerCards>
                 {data.map((i, index) => {
                     return (
-                        <Card key={index} name={i.name} city={i.city} address={i.address} />
+                        <div key={index} onClick={() => {  handleQuery(i.id)} }>
+                            <Card name={i.name} city={i.city} address={i.address} />
+                        </div>
                     )
                 })}
-                
+
             </ContainerCards>
         </Container>);
 }
